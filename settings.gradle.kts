8400e50574fff5618014d44be02d26b9fe14e770
liangyu <liangyu.chen719@gmail.com>
2025-11-18 16:14:59 +0800

    feat(app): 初始化Android项目基础结构

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+@file:Suppress("UnstableApiUsage")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    // 工具链解析,以便自动下载和使用所需的JDK版本
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lithe"
+include(":app")
