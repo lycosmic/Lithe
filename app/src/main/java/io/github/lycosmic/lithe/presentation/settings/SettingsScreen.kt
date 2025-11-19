787f80fe431f6c2034efaa448f9663e3ab58de20
liangyu <liangyu.chen719@gmail.com>
2025-11-19 20:56:39 +0800

    feat(settings): 优化设置界面UI布局与交互

--- app/src/main/java/io/github/lycosmic/lithe/presentation/settings/SettingsScreen.kt
+++ app/src/main/java/io/github/lycosmic/lithe/presentation/settings/SettingsScreen.kt
@@ -1,12 +1,16 @@
 package io.github.lycosmic.lithe.presentation.settings
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -20,10 +24,10 @@ import androidx.compose.material.icons.outlined.Tune
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -31,6 +35,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -43,7 +48,6 @@ import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SettingsScreen(
     onNavigateBack: () -> Unit,
@@ -54,8 +58,7 @@ fun SettingsScreen(
     onNavigateToBrowseSettings: () -> Unit,
     settingsManager: SettingsManager,
     modifier: Modifier = Modifier,
-
-    ) {
+) {
     // 是否是暗色模式
     val isDark by settingsManager.isDarkMode.collectAsState(initial = false)
 
@@ -79,11 +82,12 @@ fun SettingsScreen(
                 windowInsets = WindowInsets()
             )
         },
-    ) { _ ->
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .verticalScroll(rememberScrollState())
+                .padding(paddingValues = innerPadding)
+                .verticalScroll(state = rememberScrollState()),
         ) {
             SettingsItem(
                 title = "常规",
@@ -135,7 +139,8 @@ fun SettingsItem(
     icon: ImageVector,
     modifier: Modifier = Modifier,
     iconContainerColor: Color = MaterialTheme.colorScheme.surfaceContainer,
-    cornerRadius: Dp = 12.dp,
+    cornerRadius: Dp = 24.dp,
+    verticalContentPadding: Dp = 8.dp, // 垂直内间距,注意: 组件基于内容撑开
     subTitle: String? = null,
     onClick: () -> Unit
 ) {
@@ -144,6 +149,7 @@ fun SettingsItem(
             .clip(
                 shape = RoundedCornerShape(size = cornerRadius)
             )
+            .background(color = MaterialTheme.colorScheme.surfaceContainerLow) // 卡片背景色
             .clickable {
                 onClick()
             },
@@ -154,16 +160,20 @@ fun SettingsItem(
             Text(text = subTitle ?: "")
         },
         leadingContent = {
-            IconButton(
-                onClick = {},
-                colors = IconButtonDefaults.iconButtonColors().copy(
-                    containerColor = iconContainerColor
-                )
+            Surface(
+                shape = CircleShape, // 圆形图标
+                color = iconContainerColor,
+                modifier = Modifier
+                    .padding(vertical = verticalContentPadding)
+                    .size(48.dp)
             ) {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = null
-                )
+                Box(contentAlignment = Alignment.Center) {
+                    Icon(
+                        imageVector = icon,
+                        contentDescription = null,
+                        modifier = Modifier.size(28.dp)
+                    )
+                }
             }
         },
     )
