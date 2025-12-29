ce4582a0f541cf198652ba88b4950fdf7b695929
liangyu <liangyu.chen719@gmail.com>
2025-12-29 15:40:25 +0800

    feat(settings): 添加嵌套滚动支持

--- app/src/main/java/io/github/lycosmic/lithe/presentation/settings/library/LibrarySettingsScreen.kt
+++ app/src/main/java/io/github/lycosmic/lithe/presentation/settings/library/LibrarySettingsScreen.kt
@@ -37,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -133,7 +134,7 @@ fun LibrarySettingsScreen(
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
 
     Scaffold(
-        modifier = modifier,
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
             LargeTopAppBar(
                 title = {
